b0d40300702dfa636a61558a0330918c346526a8
MongolGooner <tim56@ya.ru>
2023-01-08 14:50:22 +0300

    Добавьте файлы проекта.

---
+++ UObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design_hw2
+{
+    public interface UObject
+    {
+        public object GetProperty(string key);
+        public void SetProperty(string key, object value);
+    }
+}
